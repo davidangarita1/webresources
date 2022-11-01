e7202833aea6cd2af765e87d214395149ad890d1
David Angarita <davidangarita.sistemas@gmail.com>
2022-10-31 22:31:11 -0500

    tyoe update

--- src/components/IconOption/IconOption.tsx
+++ src/components/IconOption/IconOption.tsx
@@ -2,6 +2,7 @@ import { Fragment, createElement } from "react";
 import "./IconOption.scss";
 
 import * as SiIcons from "react-icons/si";
+import { IconType } from "react-icons";
 
 type IconOptionProps = {
   iconName: string;
@@ -18,7 +19,7 @@ export const IconOption = ({
 }: IconOptionProps): JSX.Element => (
   <Fragment>
     <span style={{ fontSize: size, color: color }}>
-      {createElement(SiIcons[iconName])}
+      {createElement(SiIcons[iconName as keyof IconType])}
     </span>
     {` ${name}`}
   </Fragment>
