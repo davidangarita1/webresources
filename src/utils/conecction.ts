2376f5944fe4fdee23a435f4fef3581f9adb1fba
David Angarita <davidangarita.sistemas@gmail.com>
2023-04-06 14:27:19 -0500

    Se añade variable de entorno

--- src/utils/conecction.ts
+++ src/utils/conecction.ts
@@ -1,5 +1,5 @@
 const CLOUD = import.meta.env.VITE_APP_DATABASE_URL
 const LOCAL = import.meta.env.VITE_APP_DATABASE_LOCAL
 
-export const API_URL = LOCAL;
+export const API_URL = CLOUD;
 export const TRAKING_ID = import.meta.env.VITE_APP_TRACKING_ID
