a09d923a7916bd5ed2fc88573a8256122ff46313
David Angarita <davidangarita.sistemas@gmail.com>
2023-05-25 12:21:31 -0500

    App updated

--- src/utils/conecction.ts
+++ src/utils/conecction.ts
@@ -1,4 +1,4 @@
-/* const CLOUD = import.meta.env.VITE_APP_DATABASE_URL
-const LOCAL = import.meta.env.VITE_APP_DATABASE_LOCAL */
+const CLOUD = import.meta.env.VITE_APP_DATABASE_URL
+const LOCAL = import.meta.env.VITE_APP_DATABASE_LOCAL
 
-export const API_URL = '';
+export const API_URL = CLOUD || '';
