1e1f7536c1285e6f19e3d7d452cdf7990c4c66c3
David Angarita <davidangarita.sistemas@gmail.com>
2026-03-12 16:32:11 -0500

    feat: extract STORAGE_KEYS constant
    - Create src/constants/storageKeys.ts with centralized storage key definitions
    - Use throughout app instead of magic strings
    - Improve maintainability and reduce typos

---
+++ src/constants/storageKeys.ts
@@ -0,0 +1,7 @@
+export const STORAGE_KEYS = {
+  DARK_MODE: "bookmark_dark_mode",
+  FAVORITES: "bookmark_favorites",
+  STATUSES: "bookmark_statuses",
+  LANGUAGE: "bookmark_language",
+  USER_RESOURCES: "user-resources",
+} as const
