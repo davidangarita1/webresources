331f324e3f9a7f92608014c33bc0ed85cd859a04
David Angarita <davidangarita.sistemas@gmail.com>
2026-03-12 09:58:29 -0500

    feat(hooks): expose CRUD methods in useUserResources

--- src/hooks/useUserResources.ts
+++ src/hooks/useUserResources.ts
@@ -2,5 +2,8 @@ import { useResourceStore } from "../store"
 
 export function useUserResources() {
   const userResources = useResourceStore((s) => s.userResources)
-  return { userResources }
+  const createUserResource = useResourceStore((s) => s.createUserResource)
+  const updateUserResource = useResourceStore((s) => s.updateUserResource)
+  const deleteUserResource = useResourceStore((s) => s.deleteUserResource)
+  return { userResources, createUserResource, updateUserResource, deleteUserResource }
 }
